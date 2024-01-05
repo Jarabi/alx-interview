fb119fffa76dc838fd3cca9b6dcefa5f58960862
Alex Jarabi <jarabialex@gmail.com>
2024-01-06 00:30:17 +0300

    Correction

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -23,18 +23,16 @@ def validUTF8(data):
                 # Single byte character, no need for further checks
                 continue
             elif (byte >> 5) == 0b110:
-                byte_count = 2
+                byte_count = 1
             elif (byte >> 4) == 0b1110:
-                byte_count = 3
+                byte_count = 2
             elif (byte >> 3) == 0b11110:
-                byte_count = 4
+                byte_count = 3
             else:
                 return False
-
         else:
             if (byte >> 6) != 0b10:
                 return False
-
             byte_count -= 1
 
     return byte_count == 0
