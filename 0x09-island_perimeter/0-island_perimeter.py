1f032f80a052446a1a6f9c1e278ad109839fce49
Alex Jarabi <jarabialex@gmail.com>
2024-02-07 11:37:40 +0300

    Island perimeter challenge

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -15,18 +15,45 @@ def island_perimeter(grid):
         Perimeter of island described by grid or nothing (0)
     """
 
-    rows = len(grid)
-    cols = len(grid[0])
-    ones = 0
+    height = len(grid)    # height of grid
+    width = len(grid[0])  # width of grid
+    perimeter = 0         # initial perimeter of island
 
-    grid_perimeter = (rows + cols) * 2
+    for row in range(height):
+        for col in range(width):
+            # Check if current cell is a 1
+            if grid[row][col] == 1:
 
-    for row in grid:
-        for col in range(cols):
-            if col == 1:
-                ones += 1
+                # Check if cell is on the first row
+                if row == 0:
+                    perimeter += 1
+                else:
+                    # Check if cell above is a 0 and add 1 to perimeter
+                    if grid[row - 1][col] == 0:
+                        perimeter += 1
 
-    if ones == 0:
-        return ones
+                # Check if cell is on the first column
+                if col == 0:
+                    perimeter += 1
+                else:
+                    # Check if previous cell is a 0 and add 1 to perimeter
+                    if grid[row][col - 1] == 0:
+                        perimeter += 1
 
-    return grid_perimeter - (ones * 2)
+                # Check if cell is on the last row
+                if row == height - 1:
+                    perimeter += 1
+                else:
+                    # Check if the cell below is a 0 and add 1 to perimeter
+                    if grid[row + 1][col] == 0:
+                        perimeter += 1
+
+                # Check if cell is on the last column
+                if col == width - 1:
+                    perimeter += 1
+                else:
+                    # Check if the next cell is a 0 and add 1 to perimeter
+                    if grid[row][col + 1] == 0:
+                        perimeter += 1
+
+    return perimeter
