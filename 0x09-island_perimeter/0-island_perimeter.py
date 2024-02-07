fdb23eaa17ebd06e43edc49c422da4a6ece2dd43
Alex Jarabi <jarabialex@gmail.com>
2024-02-07 09:56:32 +0300

    Island perimeter challenge

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Island perimeter
+"""
+
+
+def island_perimeter(grid):
+    """
+    Returns the primeter of the island described in "grid"
+
+    Args:
+        grid (list): list of list of integers
+
+    Returns:
+        Perimeter of island described by grid or nothing (0)
+    """
+
+    rows = len(grid)
+    cols = len(grid[0])
+    ones = 0
+
+    grid_perimeter = (rows + cols) * 2
+
+    for row in grid:
+        for col in range(cols):
+            if col == 1:
+                ones += 1
+
+    return grid_perimeter - (ones * 2)
