d2c0f338e1c859348a173e0051d485148c8a0584
Alex Jarabi <jarabialex@gmail.com>
2024-02-16 01:54:41 +0300

    Prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Prime game
+"""
+
+
+def isWinner(x, nums):
+    """
+    Determines the winner of a prime numbers game
+
+    Args:
+        x (int): The number of rounds
+        nums (list): An array
+
+    Return:
+        name of player who won the most rounds
+        None, if the winner cannot be determined
+    """
+    winner = "Ben"
+
+    return winner
